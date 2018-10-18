7855358600e082cc4f1693599f19c41c89c42946
Logan Guthrie <logan@logan.codes>
2018-10-18 12:26:51 -0700

    Adds temporary testing

--- Main.php
+++ Main.php
@@ -1,27 +1,12 @@
 <?php
 
 class Main {
+    public static $OUTPUT = '/mnt/c/Users/Logan Guthrie/Development/code/CSVConcatPHP/Output/file.csv';
 
-    public function __construct() {
-        $this->commandLineCheck();
-    }
+    public function uploadAndShareFile() {
+        $googleController = new GoogleController();
 
-    /**
-     * Defines google drive API parameters
-     */
-    private function defineScopes() {
-        define('APPLICATION_NAME', Main::$NAME);
-        define('CREDENTIALS_PATH', Main::$CREDENTIALS);
-        define('CLIENT_SECRET_PATH', Main::$SECRET);
-        define('SCOPES', implode(' ', array(
-                Google_Service_Drive::DRIVE)
-        ));
-    }
-
-    private function commandLineCheck() {
-        if (php_sapi_name() != 'cli') {
-            throw new Exception('This application must be run on the command line.');
-        }
+        $googleController->doShareFiles('1POVVj5btF7PRA7Uv7P1v4DgDEkO1MA6H-SeHHlFmbEk');
     }
 
     /**
--- concat.php
+++ concat.php
@@ -1,17 +1,17 @@
 <?php
+require_once __DIR__ . '/vendor/autoload.php';
+
+// @todo autoload custom classes
 require_once __DIR__ . '/Main.php';
 require_once __DIR__ . '/Classes/Joins.php';
+require_once __DIR__ . '/GoogleClasses/GoogleController.php';
+require_once __DIR__ . '/GoogleClasses/GoogleUploader.php';
+require_once __DIR__ . '/GoogleClasses/GoogleShare.php';
+require_once __DIR__ . '/GoogleClasses/ClientCreator.php';
+
 const INPUT = __DIR__ . '/Files/';
+const OUTPUT = '/mnt/c/Users/Logan Guthrie/Development/code/CSVConcatPHP/Output/file.csv';
 
-const OUTPUT = __DIR__ . '/Output/file.csv';
+$main = new Main();
 
-//$main = new Main();
-//
-//$array = $main->getReadArray();
-//$main->joinFiles($array, OUT);
-//
-//print_r("Done, kupo.");
-
-$joins = new Joins();
-
-$joins->removeFirstLine(INPUT . '/Daily_Dashboard_-_Expected_Apt_-_Scheduled_01d3d0d7-433c-455c-8fad-53968f681cd5.csv');
+$main->uploadAndShareFile();
