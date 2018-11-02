4ad54b01140f25dee7a63705132f38123648378c
Logan Guthrie <logan@logan.codes>
2018-11-02 16:57:30 -0700

    Moves file operations to FileOperations class

--- Classes/Combiner.php
+++ Classes/Combiner.php
@@ -1,45 +1,17 @@
 <?php
 class Combiner {
-    /**
-     * @param $inputPath
-     * @param $outputPath
-     * @param $headerPath
-     *
-     * @throws Exception
-     */
-    public function setFileArray($inputPath, $outputPath, $headerPath) {
-        $fileNamesArray = $this->putFilenamesInArray($inputPath);
-        $filesWithPathsArray = $this->setPathToFiles($fileNamesArray, $inputPath);
-
-        $this->removeFirstLineInArray($filesWithPathsArray);
-
-        $arrayWithHeader = $this->addHeaderToArray($filesWithPathsArray, $headerPath);
-
-        $this->joinFiles($arrayWithHeader, $outputPath);
-    }
 
     /**
-     * @param array $fileArray
-     */
-    private function removeFirstLineInArray(array $fileArray) {
-        foreach ($fileArray as $file) {
-            $shell = "sed -i 1d";
-
-            shell_exec($shell . ' "' . $file . '"');
-        }
-    }
-
-    /**
-     * @param array $files
+     * @param array $inputFiles
      * @param string $outputFilePath
      *
      * @throws Exception
      */
-    private function joinFiles(array $files, $outputFilePath) {
+    public function joinFiles(array $inputFiles, $outputFilePath) {
 
         $combinedFile = fopen($outputFilePath, "w+");
 
-        foreach($files as $file) {
+        foreach($inputFiles as $file) {
             $readFile = fopen($file, "r");
             // While not end of file being read
             while(!feof($readFile)) {
@@ -50,52 +22,5 @@ class Combiner {
         }
         fclose($combinedFile);
         unset($combinedFile);
-    }
-
-    /**
-     * @param $dir string Directory to files
-     *
-     * @return array | boolean
-     */
-    private function putFilenamesInArray($dir) {
-        if ($dir) {
-            return array_diff(scandir($dir, 1), array('..', '.'));
-        } else {
-            return FALSE;
-        }
-    }
-
-    /**
-     * Adds .csv file containing only headers to the front of the file array
-     *
-     * @param array $fileArray
-     * @param string $headerFilePath
-     *
-     * @return array
-     */
-    private function addHeaderToArray(array $fileArray, $headerFilePath) {
-        array_unshift($fileArray, $headerFilePath);
-
-        return $fileArray;
-    }
-
-    /**
-     * @param array $files
-     * @param $path
-     *
-     * @return array|bool
-     */
-    private function setPathToFiles(array $files, $path) {
-        if(is_array($files)) {
-            foreach ($files as &$file) {
-                $file = $path . $file;
-            }
-
-            unset($file);
-
-            return $files;
-        } else {
-            return FALSE;
-        }
     }
 }
---
+++ Classes/FileOperations.php
@@ -0,0 +1,146 @@
+<?php
+
+class FileOperations {
+    private $output;
+    private $input;
+    private $header;
+    private $inputArray;
+
+    /**
+     * FileOperations constructor.
+     * Pass in the root directory of the application
+     *
+     * @param string $root
+     */
+    public function __construct($root) {
+        $this->setOutput($root);
+        $this->setInput($root);
+        $this->setHeader($root);
+
+        $this->setInputArray();
+    }
+
+    /**
+     * @return string
+     */
+    public function getOutput() {
+        return $this->output;
+    }
+
+    /**
+     * @return string
+     */
+    public function getInput() {
+        return $this->input;
+    }
+
+    /**
+     * @return array
+     */
+    public function getInputArray() {
+        return $this->inputArray;
+    }
+
+    /**
+     * Sets the name of the output file for the month
+     */
+    private function setOutput($root) {
+        $today = date('m-Y');
+
+        $this->output = $root . '/Output/' . $today . '.csv';
+    }
+
+    private function setInput($root) {
+        $this->input = $root . '/Files/';
+    }
+
+    private function setHeader($root) {
+        $this->header = $root . '/Header/header.csv';
+    }
+
+    private function setInputArray() {
+        $inputFilenameArray = $this->getInputFilenameArray();
+        $pathedArray = $this->addPathsToInputArray($inputFilenameArray);
+
+        $this->removeHeadersFromFiles($pathedArray);
+
+        $this->inputArray = $this->addHeaderToFileArray($pathedArray);
+    }
+
+    /**
+     * Checks to see if this month's file has been created and if not creates it
+     *
+     * @throws Exception
+     */
+    public function createOutputFile() {
+        if (file_exists($this->output)) {
+            throw new Exception('Output file already exists, try deleting the current month\'s .csv and try again');
+        } else {
+            fopen($this->output, 'w') or die('Cannot create file');
+        }
+
+    }
+
+    /**
+     * Takes an array of files and deletes them
+     *
+     * @param array $filesArray
+     */
+    public function deleteFilesAfterCompleting(array $filesArray) {
+        foreach ($filesArray as $file) {
+            unlink($file);
+        }
+    }
+
+    /**
+     * @return array | boolean
+     */
+    private function getInputFilenameArray() {
+        if ($this->input) {
+            return array_diff(scandir($this->input, 1), array('..', '.'));
+        } else {
+            return FALSE;
+        }
+    }
+
+    /**
+     * @param array $inputFilenameArray
+     *
+     * @return array|bool
+     */
+    private function addPathsToInputArray(array $inputFilenameArray) {
+        if(is_array($inputFilenameArray)) {
+            foreach ($inputFilenameArray as &$file) {
+                $file = $this->input . $file;
+            }
+
+            unset($file);
+
+            return $inputFilenameArray;
+        } else {
+            return FALSE;
+        }
+    }
+
+    /**
+     * @param array $fileArray
+     */
+    private function removeHeadersFromFiles(array $fileArray) {
+        foreach ($fileArray as $file) {
+            $shell = "sed -i 1d";
+
+            shell_exec($shell . ' "' . $file . '"');
+        }
+    }
+
+    /**
+     * @param array $fileArray
+     *
+     * @return array
+     */
+    private function addHeaderToFileArray(array $fileArray) {
+        array_unshift($fileArray, $this->header);
+
+        return $fileArray;
+    }
+}
